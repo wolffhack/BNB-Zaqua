e5b3cf15ef7ddf2b102194af9ec1e7b5c1cd3071
Mario <69741055+wolffhack@users.noreply.github.com>
2024-03-05 12:55:52 -0600

    contractsEnviromenttest

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+const dotenv = require("dotenv");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+
+
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      gas: "auto", 
+    },
+    bnbBacon: {
+      url: process.env.MOONBASE_URL || 'https://rpc.testnet.moonbeam.network',
+      chainId: 1287,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      gasPrice: 1100000000,
+    },
+  }
 };
